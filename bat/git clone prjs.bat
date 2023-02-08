be7c79a7c77d97fb9e30f2d4dfc3ac9b0533aafc
PARK4139 <https://github.com/PARK4139/jungHoonParkMapForMemorizeAllAsSoonAsPossible.github.io>
2023-02-08 09:13:40 +0900

    2023 02 08 09 13 40

---
+++ bat/git clone prjs.bat
@@ -0,0 +1,10 @@
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> variable defination
+chcp 65001
+@echo off
+setlocal
+for /f "delims=" %%i in ('Powershell.exe get-date -Format 'yyyy MM dd HH mm ss'') do set yyyyMMddHHmmss=%%i
+cls
+git clone https://github.com/PARK4139/PLAY_GROUND.git
+git clone https://github.com/PARK4139/prj_util_batch_2023.git
+git clone https://github.com/PARK4139/prj_AI_img_classifier.git
+git clone https://github.com/PARK4139/prj_mgr.git
